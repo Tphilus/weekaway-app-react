c9c247eec3e7e03b18737dd141f72e60eec62da7
Tphilus <tkdadzie@st.ug.edu.gh>
2023-05-18 17:07:40 +0000

    Booking components

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -2,25 +2,35 @@ import React from "react";
 
 const Activities = () => {
   return (
-    <div className="max-w-[1000px] m-auto md:flex mt-[-75px]">
+    <div className="max-w-[1140px] m-auto md:flex mt-[-75px]">
       <div className="relative p-4">
-        <h3 className="absolute z-10 top-[50] translate-x-[-50%]">Resorts</h3>
+        <h3 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">
+          Resorts
+        </h3>
         <img
+          className="w-full h-full md:w-[400px] md:h-[200px] object-cover relative border-4 border-white shadow-lg"
           src="https://images.unsplash.com/photo-1523365154888-8a758819b722?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80"
           alt="1st img"
         />
       </div>
+
       <div className="relative p-4">
-        <h3 className="absolute z-10 top-[50] translate-x-[-50%] ">Resorts</h3>
+        <h3 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">
+          Resorts
+        </h3>
         <img
+          className="w-full h-full md:w-[400px] md:h-[200px] object-cover relative border-4 border-white shadow-lg"
           src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=873&q=80"
           alt="1st img"
         />
       </div>
 
       <div className="relative p-4">
-        <h3 className="absolute z-10 top-[50]translate-x-[-50%]">Resorts</h3>
+        <h3 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">
+          Resorts
+        </h3>
         <img
+          className="w-fu h-full md:w-[400px] md:h-[200px] object-cover relative border-4 border-white shadow-lg"
           src="https://images.unsplash.com/photo-1483068919052-4ade7bdc0edb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=874&q=80"
           alt="1st img"
         />
---
+++ src/components/Booking.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Booking = () => {
+  return (
+    <div id="deals">
+      <form>
+        <div>
+          <label>Destination</label>
+          <select name="" id="">
+            <option value="">Grande Antigua</option>
+            <option value="">Kay West</option>
+            <option value="">Maldives</option>
+            <option value="">Cozumel</option>
+          </select>
+        </div>
+        <div>
+          <div>
+            <label>Check-In</label>
+            <input type="text" name="" id="" />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Booking;
